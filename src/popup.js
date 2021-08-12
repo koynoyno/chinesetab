5b7722fc49d855bd1f1d91909b19afdea5f46e41
Vlad Volkov <vlad.v@taskworld.com>
2021-08-12 19:06:45 +0700

    popup redraw bugfix

--- src/popup.js
+++ src/popup.js
@@ -19,15 +19,33 @@ let saveSettings = (id, checkbox = false) => {
 
   // console.log(`Set ${id}: ${value}`);
 
+  // fallback to HSK6 from HSK7-9 when switching to HSK 2.0
+  if (value == "hsk2" && document.getElementById("level").value == "hsk7-9") {
+    chrome.storage.sync.set({ level: "hsk6" });
+  }
+
+  // redraw index.html after storage.sync.set
+  // TODO: add dynamic style changing for pinyin/translation/darkMode
+  // See: https://stackoverflow.com/questions/38561136/chrome-extension-to-change-dom-with-a-button-in-extension-popup
   chrome.tabs.reload();
 
+  // redraw popup if darkMode is toggled
   if (id == "darkMode") {
-    console.log(`i check that, value=${value}`)
+    console.log(`i check that, value=${value}`);
     if (value) {
       document.body.classList.add("dark-mode");
     } else {
       document.body.classList.remove("dark-mode");
     }
+  }
+
+  // redraw #level options if HSK version is toggled
+  if (id == "hsk") {
+    level = document.querySelector("#level");
+    while (level.lastElementChild) {
+      level.removeChild(level.lastElementChild);
+    }
+    redrawHSKLevels(value);
   }
 };
 
@@ -45,6 +63,7 @@ let restoreSettings = () => {
       darkMode: darkMode,
     },
     (items) => {
+      redrawHSKLevels(items.hsk);
       // console.log(items);
       document.getElementById("hsk").value = items.hsk;
       document.getElementById("level").value = items.level;
@@ -58,6 +77,35 @@ let restoreSettings = () => {
   );
 
   // console.log(`Settings restored!`);
+};
+
+let redrawHSKLevels = (hsk) => {
+  if (hsk == "hsk2") {
+    document
+      .querySelector("#level")
+      .insertAdjacentHTML(
+        "afterbegin",
+        '<option value="hsk1">HSK 1 / 150 words</option>' +
+          '<option value="hsk2">HSK 2 / 150 words</option>' +
+          '<option value="hsk3">HSK 3 / 300 words</option>' +
+          '<option value="hsk4">HSK 4 / 600 words</option>' +
+          '<option value="hsk5">HSK 5 / 1300 words</option>' +
+          '<option value="hsk6">HSK 6 / 2500 words</option>'
+      );
+  } else {
+    document
+      .querySelector("#level")
+      .insertAdjacentHTML(
+        "afterbegin",
+        '<option value="hsk1">HSK 1 / 500 words</option>' +
+          '<option value="hsk2">HSK 2 / 772 words</option>' +
+          '<option value="hsk3">HSK 3 / 973 words</option>' +
+          '<option value="hsk4">HSK 4 / 1000 words</option>' +
+          '<option value="hsk5">HSK 5 / 1071 words</option>' +
+          '<option value="hsk6">HSK 6 / 1140 words</option>' +
+          '<option value="hsk7-9">HSK 7-9 / 5636 words</option>'
+      );
+  }
 };
 
 // multiple size="6"
